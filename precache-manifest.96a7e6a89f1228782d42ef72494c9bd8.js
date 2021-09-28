c83a5d54a6a24ce2140969a258de4feefc01e56a
Harshpal Judge <65704494+harshpaljudge@users.noreply.github.com>
2021-09-28 18:27:30 +0530

    My First Commit

---
+++ precache-manifest.96a7e6a89f1228782d42ef72494c9bd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a69907753bed4d8113a1cfe9ac812ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f644ad816157bfad2149",
+    "url": "/static/css/main.5b652384.chunk.css"
+  },
+  {
+    "revision": "8ba1fbc7de4ac605c099",
+    "url": "/static/js/2.f79b722d.chunk.js"
+  },
+  {
+    "revision": "f644ad816157bfad2149",
+    "url": "/static/js/main.4e6ba143.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
